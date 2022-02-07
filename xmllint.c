AERA-Recovery/android_external_libxml2

commit 8a28a3f2d345b2c369a758bade1ad4654ac8fe77

Don't ignore xmllint options after "-"
There's no reason to ignore options after "-". This was probably
confused with the "--" mechanism which xmllint doesn't implement.

Fixes #290.

xmllint.c
@@ -3181,8 +3181,6 @@ main(int argc, char **argv) {
 
     /* xmlMemSetup must be called before initializing the parser. */
     for (i = 1; i < argc ; i++) {
-	if (!strcmp(argv[i], "-"))
-	    break;
 	if (argv[i][0] != '-')
 	    continue;
 
@@ -3200,11 +3198,9 @@ main(int argc, char **argv) {
     LIBXML_TEST_VERSION
 
     for (i = 1; i < argc ; i++) {
-	if (!strcmp(argv[i], "-"))
-	    break;
-
 	if (argv[i][0] != '-')
 	    continue;
+
 	if ((!strcmp(argv[i], "-debug")) || (!strcmp(argv[i], "--debug")))
 	    debug++;
 	else

commit 1e1aef5832f1218da89086d997304e8e1e4501e3

Fix "xmllint -"
Short-lived regression from commit 8a28a3f2.

xmllint.c
@@ -3198,7 +3198,7 @@ main(int argc, char **argv) {
     LIBXML_TEST_VERSION
 
     for (i = 1; i < argc ; i++) {
-	if (argv[i][0] != '-')
+	if (argv[i][0] != '-' || argv[i][1] == 0)
 	    continue;
 
 	if ((!strcmp(argv[i], "-debug")) || (!strcmp(argv[i], "--debug")))