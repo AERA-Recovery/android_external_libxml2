AERA-Recovery/android_external_libxml2

commit b394f40db111cd33cb408d6e0b7f8118428af87e

Fix unused variable warning in python/types.c

python/types.c
@@ -82,8 +82,11 @@ libxml_PyFileGet(PyObject *f) {
     FILE *res;
     const char *mode;
 
-    int fd = PyObject_AsFileDescriptor(f);
+#ifdef _WIN32
     intptr_t w_fh = -1;
+#else
+    int fd = PyObject_AsFileDescriptor(f);
+#endif
 
 #ifdef _WIN32
     HMODULE hntdll = NULL;

commit 3a3b5c0e98fe0430223a1e50c2e64db71e816d8e

Fix libxml_PyFileGet
Broken in commit b394f40d.

python/types.c
@@ -81,14 +81,10 @@ libxml_PyFileGet(PyObject *f) {
     int flags;
     FILE *res;
     const char *mode;
+    int fd = PyObject_AsFileDescriptor(f);
 
 #ifdef _WIN32
     intptr_t w_fh = -1;
-#else
-    int fd = PyObject_AsFileDescriptor(f);
-#endif
-
-#ifdef _WIN32
     HMODULE hntdll = NULL;
     IO_STATUS_BLOCK status_block;
     FILE_ACCESS_INFORMATION ai;