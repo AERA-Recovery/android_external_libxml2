AERA-Recovery/android_external_libxml2

commit dc87784e2c8e6669c1c8709993ab6d345229b9e7

parser: Fix push parser with unterminated CDATA sections
Short-lived regression found by OSS-Fuzz.

parser.c
@@ -11794,20 +11794,24 @@ xmlParseTryOrFinish(xmlParserCtxtPtr ctxt, int terminate) {
                      */
                     term = BAD_CAST strstr((const char *) ctxt->input->cur,
                                            "]]>");
-                    if (term == NULL)
-                        term = ctxt->input->end;
                 } else {
 		    term = xmlParseLookupString(ctxt, 0, "]]>", 3);
                 }
 
 		if (term == NULL) {
-		    int tmp;
+		    int tmp, size;
 
-		    if (avail < XML_PARSER_BIG_BUFFER_SIZE + 2)
-		        goto done;
-                    ctxt->checkIndex = 0;
-                    tmp = xmlCheckCdataPush(ctxt->input->cur,
-                                            XML_PARSER_BIG_BUFFER_SIZE, 0);
+                    if (terminate) {
+                        /* Unfinished CDATA section */
+                        size = ctxt->input->end - ctxt->input->cur;
+                    } else {
+                        if (avail < XML_PARSER_BIG_BUFFER_SIZE + 2)
+                            goto done;
+                        ctxt->checkIndex = 0;
+                        /* XXX: Why don't we pass the full buffer? */
+                        size = XML_PARSER_BIG_BUFFER_SIZE;
+                    }
+                    tmp = xmlCheckCdataPush(ctxt->input->cur, size, 0);
                     if (tmp < 0) {
                         tmp = -tmp;
                         ctxt->input->cur += tmp;

commit 077c39b1a1e6aed7ed6d76e8126982524a66f4fa

parser: Fix infinite loop with push parser in recovery mode
Short-lived regression from commit dc87784e. Found by OSS-Fuzz.

parser.c
@@ -11836,7 +11836,7 @@ xmlParseTryOrFinish(xmlParserCtxtPtr ctxt, int terminate) {
                         size = XML_PARSER_BIG_BUFFER_SIZE;
                     }
                     tmp = xmlCheckCdataPush(ctxt->input->cur, size, 0);
-                    if (tmp < 0) {
+                    if (tmp <= 0) {
                         tmp = -tmp;
                         ctxt->input->cur += tmp;
                         goto encoding_error;