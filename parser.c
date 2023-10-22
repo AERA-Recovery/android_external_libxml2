AERA-Recovery/android_external_libxml2

commit ba3def24ca9069f5b50628757b833e210878c6c3

parser: Fix regression when push parsing parameter entities
Short-lived regression from 87b486cf.

Also shrink parameter entity buffers when push parsing.

parser.c
@@ -2261,13 +2261,11 @@ static int spacePop(xmlParserCtxtPtr ctxt) {
         xmlParserGrow(ctxt);						\
   } while (0)
 
-#define SHRINK if ((ctxt->progressive == 0) &&				\
-		   (ctxt->input->cur - ctxt->input->base > 2 * INPUT_CHUNK) && \
+#define SHRINK if ((ctxt->input->cur - ctxt->input->base > 2 * INPUT_CHUNK) && \
 		   (ctxt->input->end - ctxt->input->cur < 2 * INPUT_CHUNK)) \
 	xmlParserShrink(ctxt);
 
-#define GROW if ((ctxt->progressive == 0) &&				\
-		 (ctxt->input->end - ctxt->input->cur < INPUT_CHUNK))	\
+#define GROW if (ctxt->input->end - ctxt->input->cur < INPUT_CHUNK)	\
 	xmlParserGrow(ctxt);
 
 #define SKIP_BLANKS xmlSkipBlankChars(ctxt)

parserInternals.c
@@ -519,7 +519,7 @@ xmlParserGrow(xmlParserCtxtPtr ctxt) {
     if (buf == NULL)
         return(0);
     /* Don't grow push parser buffer. */
-    if (ctxt->progressive)
+    if ((ctxt->progressive) && (ctxt->inputNr <= 1))
         return(0);
     if (buf->error != 0)
         return(-1);
@@ -603,10 +603,9 @@ xmlParserShrink(xmlParserCtxtPtr ctxt) {
     xmlParserInputBufferPtr buf = in->buf;
     size_t used;
 
-    /* Don't shrink pull parser memory buffers. */
+    /* Don't shrink push parser buffer. */
     if ((buf == NULL) ||
-        ((ctxt->progressive == 0) &&
-         (buf->encoder == NULL) && (buf->readcallback == NULL)))
+        ((ctxt->progressive) && (ctxt->inputNr <= 1)))
         return;
 
     used = in->cur - in->base;

result/valid/huge.xml
@@ -1,0 +1,6 @@
+<?xml version="1.0"?>
+<!DOCTYPE doc [
+<!ENTITY % pe SYSTEM "dtds/huge.ent">
+<!-- More than MINLEN (4000) bytes of comments --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!-- ................................................................. --><!ELEMENT doc (#PCDATA)>
+]>
+<doc>test</doc>

result/valid/pe-latin1.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0"?>
+<!DOCTYPE doc [
+<!ELEMENT doc (#PCDATA)>
+<!ENTITY % pe SYSTEM "dtds/pe-latin1.ent">
+<!ENTITY latin1 "äöü">
+]>
+<doc>&latin1;</doc>

test/valid/dtds/huge.ent
@@ -1,0 +1,104 @@
+<!-- More than MINLEN (4000) bytes of comments -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+<!-- ................................................................. -->
+
+<!ELEMENT doc (#PCDATA)>

test/valid/dtds/pe-latin1.ent
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!ENTITY latin1 "äöü">

test/valid/huge.xml
@@ -1,0 +1,5 @@
+<!DOCTYPE doc [
+<!ENTITY % pe SYSTEM "dtds/huge.ent">
+%pe;
+]>
+<doc>test</doc>

test/valid/pe-latin1.xml
@@ -1,0 +1,6 @@
+<!DOCTYPE doc [
+<!ELEMENT doc (#PCDATA)>
+<!ENTITY % pe SYSTEM "dtds/pe-latin1.ent">
+%pe;
+]>
+<doc>&latin1;</doc>

commit 19c6abe9f196c9028046bf838009c8c3ba80ffd0

parser: Fix buffer shrinking when push parsing
Short-lived regression from ba3def24.

parser.c
@@ -2261,8 +2261,11 @@ static int spacePop(xmlParserCtxtPtr ctxt) {
         xmlParserGrow(ctxt);						\
   } while (0)
 
-#define SHRINK if ((ctxt->input->cur - ctxt->input->base > 2 * INPUT_CHUNK) && \
-		   (ctxt->input->end - ctxt->input->cur < 2 * INPUT_CHUNK)) \
+/* Don't shrink push parser buffer. */
+#define SHRINK \
+    if (((ctxt->progressive == 0) || (ctxt->inputNr > 1)) && \
+        (ctxt->input->cur - ctxt->input->base > 2 * INPUT_CHUNK) && \
+	(ctxt->input->end - ctxt->input->cur < 2 * INPUT_CHUNK)) \
 	xmlParserShrink(ctxt);
 
 #define GROW if (ctxt->input->end - ctxt->input->cur < INPUT_CHUNK)	\

parserInternals.c
@@ -603,9 +603,7 @@ xmlParserShrink(xmlParserCtxtPtr ctxt) {
     xmlParserInputBufferPtr buf = in->buf;
     size_t used;
 
-    /* Don't shrink push parser buffer. */
-    if ((buf == NULL) ||
-        ((ctxt->progressive) && (ctxt->inputNr <= 1)))
+    if (buf == NULL)
         return;
 
     used = in->cur - in->base;